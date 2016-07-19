a8b24bbad37a24f62975cbf6e7fdf27718f18252
zodiacon <zodiacon@live.com>
2016-07-19 09:56:07 +0300

    packed as a single executable file

--- QSlice/App.xaml.cs
+++ QSlice/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 using QSlice.ViewModels;
@@ -12,6 +13,40 @@ namespace QSlice {
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        public App() {
+            LoadAssemblies();
+        }
+
+        readonly Dictionary<string, Assembly> _assemblies = new Dictionary<string, Assembly>();
+
+
+
+        private void LoadAssemblies() {
+            var appAssembly = typeof(App).Assembly;
+            foreach(var resourceName in appAssembly.GetManifestResourceNames()) {
+                if(resourceName.EndsWith(".dll", StringComparison.InvariantCultureIgnoreCase)) {
+                    using(var stream = appAssembly.GetManifestResourceStream(resourceName)) {
+                        var assemblyData = new byte[(int)stream.Length];
+                        stream.Read(assemblyData, 0, assemblyData.Length);
+                        var assembly = Assembly.Load(assemblyData);
+                        _assemblies.Add(assembly.GetName().Name, assembly);
+                    }
+                }
+            }
+            AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
+        }
+
+
+
+        Assembly OnAssemblyResolve(object sender, ResolveEventArgs args) {
+            var shortName = new AssemblyName(args.Name).Name;
+            Assembly assembly;
+            if(_assemblies.TryGetValue(shortName, out assembly)) {
+                return assembly;
+            }
+            return null;
+        }
+
         MainViewModel _mainViewModel;
 
         protected override void OnStartup(StartupEventArgs e) {
